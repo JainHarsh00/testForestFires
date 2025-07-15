a3c72216259346176f98376f57d562626d429b15
JainHarsh00 <jainharsh4740@gmail.com>
2025-07-15 17:06:09 +0530

    Attempted to keep page alive

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 from flask import Flask, request, render_template
 import numpy as np
@@ -47,4 +48,4 @@ def predict_datapoint():
     return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
